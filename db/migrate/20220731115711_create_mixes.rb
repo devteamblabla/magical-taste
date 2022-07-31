bfe578284cafe951d3976fad77e80aad07fb1604
denis <denisdenis9331@gmail.com>
2022-07-31 17:04:08 +0500

    create table mix

---
+++ db/migrate/20220731115711_create_mixes.rb
@@ -0,0 +1,10 @@
+class CreateMixes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :mixes do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
