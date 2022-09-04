8fb95b08b57c217ff7d0984edb372fb6fc9f283d
denis <denisdenis9331@gmail.com>
2022-09-04 22:19:02 +0500

    feature: database prepopulation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Mix.create(:name => "Orange Soda Horchata", :description => "Orange Soda 60% + Horchata 40%")
